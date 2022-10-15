ec52c406ff956c8266a729ba9b6a5b415792e29b
Deny Anwar <denyanwardev@gmail.com>
2022-10-15 18:27:29 +0800

    done exercise chapter 5 functional

--- basic/chapter_5_functional_programming/exercise/exercise_functional.js
+++ basic/chapter_5_functional_programming/exercise/exercise_functional.js
@@ -22,11 +22,10 @@
   ];
   
   // TODO
-  
-  
-  /**
-   * Jangan hapus kode di bawah ini
-   */
+ const greatAuthors = books.filter((book) => book.sales > 1000000)
+                           .map((book) => {return `${book.author} adalah penulis buku ${book.title} yang sangat hebat!`});
+
+ console.log(greatAuthors);
   
   module.exports = { books, greatAuthors };
   
