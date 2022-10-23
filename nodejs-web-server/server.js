f2af26366809913a96e1dde1e293d75021be9e3e
Deny Anwar <denyanwardev@gmail.com>
2022-10-23 20:44:29 +0800

    add new project, modified a number of files

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -7,28 +7,56 @@ const http = require('http');
  * @param response: objek yang digunakan untuk menanggapi permintaan
  */
 const requestListener = (request, response) => {
-    const { method } = request.method;
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
 
-    if (method === 'GET') {
-        // response ketika GET
-        
+    const { method, url } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: `Ini adalah homepage`,
+            }));
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request.`,
+            }));
+        }
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: `Halo! Ini adalah halaman about.`,
+            }));
+        } else if (method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about.`,
+                }));
+            });
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request.`,
+            }));
+        }
+    } else {
+        response.statusCode = 404;
+        response.end(JSON.stringify({
+            messaage: 'Halaman tidak ditemukan!',
+        }));
     }
-
-    if (method === 'POST') {
-        // response ketika POST
-    }
-
-    if (mehod === 'PUT') {
-        // response ketika PUT
-    }
-
-    if (mehod === 'DELETE') {
-        // response ketika DELETE
-    }
-
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
 };
 
 const server = http.createServer(requestListener);
